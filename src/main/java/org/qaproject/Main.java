14c2ac606f1d976d35c77dc116fa854d3e98a4ad
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-03-13 16:15:24 +0200

    Editing xPath

--- src/main/java/org/qaproject/Main.java
+++ src/main/java/org/qaproject/Main.java
@@ -1,6 +1,9 @@
+// ЭТОТ КЛАСС ЯВЛЯЕТЬСЯ ЧАСТЬЮ ПРОЕКТА ДЛЯ ПОРТФОЛИО
 package org.qaproject;
 // Импорт библиотек
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -19,7 +22,11 @@ public class Main {
         WebDriver driver = new ChromeDriver(options);
 
         // Открытие страницы
-        driver.get("https://www.youtube.com/");
+          driver.get("https://www.google.com/");
+
+        // создание локатора типа xPath
+        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+        input.click();
 
         // Закрытие браузера
         driver.quit();
