0c10003a09a8cb03821bfbd847367620aa3ae127
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-03-15 20:38:07 +0200

    Editing xPath

--- src/main/java/org/qaproject/Main.java
+++ src/main/java/org/qaproject/Main.java
@@ -1,11 +1,13 @@
 // ЭТОТ КЛАСС ЯВЛЯЕТЬСЯ ЧАСТЬЮ ПРОЕКТА ДЛЯ ПОРТФОЛИО
 package org.qaproject;
 // Импорт библиотек
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,14 +24,23 @@ public class Main {
         WebDriver driver = new ChromeDriver(options);
 
         // Открытие страницы
-          driver.get("https://www.google.com/");
+          driver.get("https://aliexpress.ru/");
 
         // создание локатора типа xPath
-        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
-        input.click();
+        WebElement inputField = driver.findElement(By.id("searchInput"));
+        inputField.clear();
+        inputField.sendKeys("Кросовки");
+        inputField.sendKeys(Keys.ENTER);
 
+
+        WebElement goods = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[2]/div[2]/div/div/div/div[1]/div/a/div/div[3]"));
+        goods.click();
+
+
+        WebElement buy = driver.findElement(By.xpath("//*[@id=\"buyNowButton\"]/div/div[1]/div[2]/div/button"));
+        buy.click();
         // Закрытие браузера
-        driver.quit();
+       // driver.quit();
     }
 
 }
