43ccde8653793ca511ae3668a9f6b3198a56f17d
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2023-04-25 18:42:38 +0300

    Work with TestNG

--- src/main/java/org/qaproject/Main.java
+++ src/main/java/org/qaproject/Main.java
@@ -5,35 +5,54 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Main {
 
-    public static void main(String[] args) {
+    WebDriver driver;
+    ChromeOptions options;
+    String url = "https://www.amazon.com";
+    String chromeParameters = "--remote-allow-origins=*";
+    String pathToChromeDriver = "D:\\MyProjects\\MyProject\\src\\main\\resources\\chromeWebDriver\\chromedriver.exe";
 
-
-            // Установка пути к драйверу Chrome
-            System.setProperty("webdriver.chrome.driver","D:\\MyProjects\\MyProject\\src\\main\\resources\\chromeWebDriver\\chromedriver.exe");
-
-            // Создание объекта ChromeOptions
-            ChromeOptions options = new ChromeOptions();
-
-            // Задание аргумента
-            options.addArguments("--remote-allow-origins=*");
-
-            // Создание объекта WebDriver с настройками ChromeOptions
-            WebDriver driver = new ChromeDriver(options);
-
-            // Открытие страницы
-            driver.get("https://www.amazon.com");
-
-            // создание локатора типа xPath
-            driver.findElement(By.xpath("//a[@id='nav-hamburger-menu']")).click();
-            int size = driver.findElements(By.xpath("//div[contains(text(), 'Electronics')]")).size();
-            //assert.assertEquals(size, 1);
-
-            // Закрытие браузера
-            // driver.quit();
-
-
+    @Test
+    public void existComputers(){
+        // Установка пути к драйверу Chrome
+        System.setProperty("webdriver.chrome.driver",pathToChromeDriver);
+        // Создание объекта ChromeOptions
+        options = new ChromeOptions();
+        // Задание аргумента
+        options.addArguments(chromeParameters);
+        // Создание объекта WebDriver с настройками ChromeOptions
+         driver = new ChromeDriver(options);
+        // Открытие страницы
+        driver.get(url);
+        // создание локатора типа xPath
+        driver.findElement(By.xpath("//a[@id='nav-hamburger-menu']")).click();
+        int size = driver.findElements(By.xpath("//div[contains(text(), 'Computers')]")).size();
+        Assert.assertEquals(size, 1);
+        // Закрытие браузера
+        driver.quit();
+    }
+    @Test
+    public void existElectronics(){
+        // Установка пути к драйверу Chrome
+        System.setProperty("webdriver.chrome.driver",pathToChromeDriver);
+        // Создание объекта ChromeOptions
+        options = new ChromeOptions();
+        // Задание аргумента
+        options.addArguments(chromeParameters);
+        // Создание объекта WebDriver с настройками ChromeOptions
+        driver = new ChromeDriver(options);
+        // Открытие страницы
+        driver.get(url);
+        // создание локатора типа xPath
+        driver.findElement(By.xpath("//a[@id='nav-hamburger-menu']")).click();
+        int size = driver.findElements(By.xpath("//div[contains(text(), 'Electronics')]")).size();
+        Assert.assertEquals(size, 1);
+        // Закрытие браузера
+        driver.quit();
     }
 
 }
